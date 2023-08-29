99ed6f0dc054680712e0f0c39ab07c02efe28a1d
1i1-tecca <137686989+1i1-tecca@users.noreply.github.com>
2023-08-29 19:04:10 +0300

    Delete task_30.py

--- task_30.py
+++
@@ -1,5 +0,0 @@
-a1 = int(input('Введите первый элемент: '))
-d = int(input('Введите разность: '))
-n = int(input('Введите количество элементов: '))
-for i in range(n):
-    print(a1 + i * d, end=' ')
